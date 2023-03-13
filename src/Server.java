eaaf9aa8c6cc4299079e10da04c2430a32a294ac
Albina Lapchevska <albina.lapchevska@gmail.com>
2023-03-13 16:15:52 +0100

    Initial commit

---
+++ src/Server.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class Server extends JFrame {
+    Server() {
+        setSize(450, 700);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new Server();
+    }
+}
