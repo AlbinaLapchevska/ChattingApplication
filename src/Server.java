2d7e2790a3e2586afc0e0350a2016ce092815539
Albina Lapchevska <albina.lapchevska@gmail.com>
2023-03-15 09:09:00 +0100

    Design server screen

--- src/Server.java
+++ src/Server.java
@@ -1,13 +1,17 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
-public class Server extends JFrame {
+public class Server extends JFrame implements ActionListener {
     Server() {
         setLayout(null);
 
         JPanel p1 = new JPanel();
-        p1.setBackground(new Color(1, 129, 255));
-        p1.setBounds(0,0,450,70);
+        p1.setBackground(new Color(3, 106, 208));
+        p1.setBounds(0, 0, 450, 70);
         p1.setLayout(null);
         add(p1);
 
@@ -18,14 +22,84 @@ public class Server extends JFrame {
         back.setBounds(5, 20, 25, 25);
         p1.add(back);
 
+        back.addMouseListener(new MouseAdapter() {
+            public void mouseClicked(MouseEvent ae) {
+                setVisible(false);
+                System.exit(0);
+            }
+        });
+
+        ImageIcon i4 = new ImageIcon(ClassLoader.getSystemResource("icons/1.png"));
+        Image i5 = i4.getImage().getScaledInstance(50, 50, Image.SCALE_DEFAULT);
+        ImageIcon i6 = new ImageIcon(i5);
+        JLabel profile = new JLabel(i6);
+        profile.setBounds(40, 10, 50, 50);
+        p1.add(profile);
+
+        ImageIcon i7 = new ImageIcon(ClassLoader.getSystemResource("icons/video.png"));
+        Image i8 = i7.getImage().getScaledInstance(30, 30, Image.SCALE_DEFAULT);
+        ImageIcon i9 = new ImageIcon(i8);
+        JLabel video = new JLabel(i9);
+        video.setBounds(300, 20, 30, 30);
+        p1.add(video);
+
+        ImageIcon i10 = new ImageIcon(ClassLoader.getSystemResource("icons/phone.png"));
+        Image i11 = i10.getImage().getScaledInstance(35, 30, Image.SCALE_DEFAULT);
+        ImageIcon i12 = new ImageIcon(i11);
+        JLabel phone = new JLabel(i12);
+        phone.setBounds(350, 20, 35, 30);
+        p1.add(phone);
+
+        ImageIcon i13 = new ImageIcon(ClassLoader.getSystemResource("icons/3icon.png"));
+        Image i14 = i13.getImage().getScaledInstance(10, 25, Image.SCALE_DEFAULT);
+        ImageIcon i15 = new ImageIcon(i14);
+        JLabel more = new JLabel(i15);
+        more.setBounds(400, 20, 10, 25);
+        p1.add(more);
+
+        JLabel name = new JLabel("James");
+        name.setBounds(110, 15, 100, 18);
+        name.setForeground(Color.WHITE);
+        name.setFont(new Font("SAN_SERIF", Font.BOLD, 18));
+        p1.add(name);
+
+        JLabel status = new JLabel("Active Now");
+        status.setBounds(110, 35, 100, 18);
+        status.setForeground(Color.WHITE);
+        status.setFont(new Font("SAN_SERIF", Font.BOLD, 14));
+        p1.add(status);
+
+        JPanel a1 = new JPanel();
+        a1.setBounds(5, 75, 440, 570);
+        add(a1);
+
+        JTextField text = new JTextField();
+        text.setBounds(5, 655, 310, 40);
+        text.setFont(new Font("SAN_SERIF", Font.PLAIN, 16));
+        add(text);
+
+        JButton send = new JButton("Send");
+        send.setBounds(320, 655, 123, 40);
+        send.setBackground(new Color(3, 106, 208));
+        send.setForeground(Color.WHITE);
+        send.addActionListener(this);
+        send.setFont(new Font("SAN_SERIF", Font.PLAIN, 16));
+        add(send);
+
         setSize(450, 700);
+        setUndecorated(true);
         setLocation(250, 150);
-        getContentPane().setBackground(new Color(248, 249, 252));
+        getContentPane().setBackground(new Color(87, 170, 252));
 
         setVisible(true);
     }
 
     public static void main(String[] args) {
         new Server();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent ae) {
+
     }
 }
